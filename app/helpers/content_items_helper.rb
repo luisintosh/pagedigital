3412eb68593bb0c5d8aeee41801a20b707a74d1f
Luis Mendieta <luismepa@gmail.com>
2022-06-19 18:15:28 -0500

    feat: style content items

--- app/helpers/content_items_helper.rb
+++ app/helpers/content_items_helper.rb
@@ -1,2 +1,7 @@
 module ContentItemsHelper
+  def content_item_image_tag(content_item, options)
+    placeholder = "https://via.placeholder.com/#{options[:size]}.png"
+    image_path = content_item.custom_thumbnail_image.representable? ? content_item.custom_thumbnail_image : placeholder
+    image_tag(image_path, { size: options[:size] })
+  end
 end
