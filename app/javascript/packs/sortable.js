ad86ab9692dc222467fb06f2cbfa9d0e2017d620
Luis Mendieta <luismepa@gmail.com>
2022-07-10 19:45:31 -0500

    fix: sortable lib error

--- app/javascript/packs/sortable.js
+++ app/javascript/packs/sortable.js
@@ -1,24 +1,28 @@
 import Sortable from 'sortablejs'
 
-const el = document.getElementById('page_content_items')
-const sortable = Sortable.create(
-    el,
-    {
-        handle: '.content-item-move-icon',
-        easing: 'cubic-bezier(1, 0, 0, 1)',
-        ghostClass: 'sortable-ghost',
-        onUpdate: (event) => {
-            console.log(event);
-            const element = event.item;
-            const elementHandler = element.querySelector('.content-item-move-icon');
-            const pathToUpdate = elementHandler.dataset.reorderHref;
-            if (pathToUpdate) {
-                console.log(pathToUpdate);
-                const csrfToken = document.querySelector('meta[name=csrf-token]')?.content;
-                const headers = {'X-CSRF-Token': csrfToken, 'Content-Type': 'application/json'}
-                const body = JSON.stringify({destination: event.newIndex});
-                fetch(pathToUpdate, {method: 'PATCH', headers, body}).catch(console.error);
+document.addEventListener("turbo:load", function() {
+    const el = document.getElementById('page_content_items')
+    if (el) {
+        Sortable.create(
+            el,
+            {
+                handle: '.content-item-move-icon',
+                easing: 'cubic-bezier(1, 0, 0, 1)',
+                ghostClass: 'sortable-ghost',
+                onUpdate: (event) => {
+                    // console.log(event);
+                    const element = event.item;
+                    const elementHandler = element.querySelector('.content-item-move-icon');
+                    const pathToUpdate = elementHandler.dataset.reorderHref;
+                    if (pathToUpdate) {
+                        // console.log(pathToUpdate);
+                        const csrfToken = document.querySelector('meta[name=csrf-token]')?.content;
+                        const headers = {'X-CSRF-Token': csrfToken, 'Content-Type': 'application/json'}
+                        const body = JSON.stringify({destination: event.newIndex});
+                        fetch(pathToUpdate, {method: 'PATCH', headers, body}).catch(console.error);
+                    }
+                }
             }
-        }
+        )
     }
-)
+})
