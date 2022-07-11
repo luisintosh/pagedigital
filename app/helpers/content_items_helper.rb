f39b63707d056cf7bc72e22e9a5770e696ecbcfa
Luis Mendieta <luismepa@gmail.com>
2022-07-10 19:22:26 -0500

    feat: style bootstrap theme and add icons

--- app/helpers/content_items_helper.rb
+++ app/helpers/content_items_helper.rb
@@ -1,6 +1,6 @@
 module ContentItemsHelper
   def content_item_image_tag(content_item, options)
-    placeholder = "https://via.placeholder.com/#{options[:size]}.png"
+    placeholder = "link_thumbnail_placeholder.svg"
     image_path = content_item.custom_thumbnail_image.representable? ? content_item.custom_thumbnail_image : placeholder
     image_tag(image_path, { size: options[:size] })
   end
