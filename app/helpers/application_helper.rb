da8a01f1965e308ffcecd9990aac4a71eb76b4cc
Luis Mendieta <luismepa@gmail.com>
2022-07-21 23:09:54 -0500

    feat: public profile page first iteration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def attached_image_tag(attached_file, placeholder_file, options = {})
+    image_path = attached_file.representable? ? attached_file : placeholder_file
+    image_tag(image_path, options)
+  end
 end
--- app/helpers/public_helper.rb
+++ app/helpers/public_helper.rb
@@ -1,2 +1,9 @@
 module PublicHelper
+  def profile_page_header_image_url
+    if @page && @page.page_profile.header_image.representable?
+      url_for(@page.page_profile.header_image)
+    else
+      asset_path('header_image_placeholder.svg')
+    end
+  end
 end
