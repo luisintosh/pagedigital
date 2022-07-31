dec2c95af80a17f921e13ec34404e167a10497e2
Luis Mendieta <luismepa@gmail.com>
2022-07-31 12:10:52 -0500

    feat: publish, deactivate, and lock page by code

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -4,8 +4,19 @@ class PublicController < ApplicationController
   def index
   end
 
+  # GET/POST /:page_slug
   def page
-    @page = Page.includes_all.find_by!(slug: params[:page_slug])
-    render layout: 'public_page'
+    @page = Page.includes_all.find_by!(slug: page_params[:page_slug])
+    if @page.published && (@page.lock_code.empty? || @page.lock_code == page_params[:unlock_code])
+      render 'page', layout: 'public_page'
+    else
+      render 'restricted_page', status: :forbidden
+    end
+  end
+
+  private
+
+  def page_params
+    params.permit(:page_slug, :unlock_code, :authenticity_token)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
     end
   end
 
-  get ':page_slug', to: 'public#page', as: :public_page
+  match ':page_slug', to: 'public#page', as: :public_page, via: %i[get post]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
