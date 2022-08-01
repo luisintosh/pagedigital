b0dedd4074ed507f59cb1ada036573b99705fb05
Luis Mendieta <luismepa@gmail.com>
2022-08-01 16:30:34 -0500

    feat: beautify paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  resources :pages, param: :slug do
-    resource :page_profile, only: %i[edit update]
-    resource :page_appearance, only: %i[edit update]
-    resource :page_contact, only: %i[edit update]
-    resource :page_integration, only: %i[edit update]
-    resources :content_items do
-      member do
-        patch 'reorder-link'
+  scope :admin do
+    resources :pages, param: :slug, path: '/' do
+      resource :page_profile, only: %i[edit update]
+      resource :page_appearance, only: %i[edit update]
+      resource :page_contact, only: %i[edit update]
+      resource :page_integration, only: %i[edit update]
+      resources :content_items do
+        member do
+          patch 'reorder-link'
+        end
       end
     end
   end
