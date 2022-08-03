dbda4b0454ea62522aafe269383327d18b8d62a5
Luis Mendieta <luismepa@gmail.com>
2022-08-03 16:54:40 -0500

    feat: seed to create default user and page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Create default user
+user = User.create(email: 'admin@page.digital', password: 'Perreo1996')
+# Create default pages
+['admin'].each { |slug| user.pages.create( slug: )}
