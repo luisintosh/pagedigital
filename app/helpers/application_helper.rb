23709435f26e14f022e4df7d5ea6684414187a58
Luis Mendieta <luismepa@gmail.com>
2022-08-06 12:07:58 -0500

    feat: show link icon if exists

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,30 @@
 module ApplicationHelper
-  def attached_image_tag(attached_file, placeholder_file, options = {})
-    image_path = attached_file.representable? ? attached_file : placeholder_file
+  def attached_image_tag(attached_file, placeholder_path, options = {})
+    image_path = attached_file.representable? ? attached_file : placeholder_path
     image_tag(image_path, options)
+  end
+
+  def content_item_image_tag(attached_file, item_url, options = {})
+    icon_path = url_icon_path(item_url) || 'link_thumbnail_placeholder.svg'
+    attached_image_tag(attached_file, icon_path, options)
+  end
+
+  def url_icon_path(url)
+    uri = URI.parse(url)
+    hostname = uri.hostname
+    return unless hostname
+
+    hostname.slice!('www.')
+    icon_name = "icons/#{hostname}.svg"
+    has_asset?(icon_name) && icon_name
+  end
+
+  def has_asset?(path)
+    begin
+      image_path(path)
+      true
+    rescue
+      false
+    end
   end
 end
--- app/helpers/content_items_helper.rb
+++ app/helpers/content_items_helper.rb
@@ -1,7 +1,2 @@
 module ContentItemsHelper
-  def content_item_image_tag(content_item, options)
-    placeholder = "link_thumbnail_placeholder.svg"
-    image_path = content_item.custom_thumbnail_image.representable? ? content_item.custom_thumbnail_image : placeholder
-    image_tag(image_path, { size: options[:size] })
-  end
 end
