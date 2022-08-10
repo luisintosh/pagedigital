f6c658abab83601c4391a8940807ec72aa503b3b
Luis Mendieta <luismepa@gmail.com>
2022-08-09 19:52:48 -0500

    fix: content item ideas icons

--- app/models/content_ideas/items.rb
+++ app/models/content_ideas/items.rb
@@ -5,44 +5,44 @@ module ContentIdeas
         title: 'Share your content',
         subtitle: 'Share content directly on your Page.digital',
         links: [
-          { title: 'Youtube', subtitle: 'Share YouTube videos on your Page.digital.', icon: 'icons/youtube.svg', link: 'https://support.google.com/youtube/answer/57741' },
-          { title: 'Tiktok', subtitle: 'Grow your TikTok following.', icon: 'icons/tiktok.svg', link: 'https://support.tiktok.com/using-tiktok/exploring-videos/sharing' },
-          { title: 'Instagram', subtitle: 'Display your beautiful photos.', icon: 'icons/instagram.svg', link: 'https://instagram.com/' },
-          { title: 'Twitter', subtitle: 'Showcase your tweets and Twitter feed.', icon: 'icons/twitter.svg', link: 'https://twitter.com/' },
-          { title: 'Twitch', subtitle: 'Get more stream viewers and engagement.', icon: 'icons/twitch.svg', link: 'https://twitch.tv/' },
-          { title: 'Facebook', subtitle: 'Add Facebook posts to your Page.digital.', icon: 'icons/facebook.svg', link: 'https://www.facebook.com/' }
+          { title: 'Youtube', subtitle: 'Share YouTube videos on your Page.digital.', icon: 'icons/youtube.com.svg', link: 'https://support.google.com/youtube/answer/57741' },
+          { title: 'Tiktok', subtitle: 'Grow your TikTok following.', icon: 'icons/tiktok.com.svg', link: 'https://support.tiktok.com/using-tiktok/exploring-videos/sharing' },
+          { title: 'Instagram', subtitle: 'Display your beautiful photos.', icon: 'icons/instagram.com.svg', link: 'https://instagram.com/' },
+          { title: 'Twitter', subtitle: 'Showcase your tweets and Twitter feed.', icon: 'icons/twitter.com.svg', link: 'https://twitter.com/' },
+          { title: 'Twitch', subtitle: 'Get more stream viewers and engagement.', icon: 'icons/twitch.tv.svg', link: 'https://twitch.tv/' },
+          { title: 'Facebook', subtitle: 'Add Facebook posts to your Page.digital.', icon: 'icons/facebook.com.svg', link: 'https://www.facebook.com/' }
         ]
       },
       {
         title: 'Share your music',
         subtitle: 'Get more followers to your musical project',
         links: [
-          { title: 'Spotify', subtitle: 'Share your songs or podcasts.', icon: 'icons/spotify.svg', link: 'https://artists.spotify.com/help/article/sharing-your-music' },
-          { title: 'Apple music', subtitle: 'Get more listeners to your songs.', icon: 'icons/apple.svg', link: 'https://support.apple.com/guide/music-web/share-music-apdm0783785d/web' },
-          { title: 'Soundcloud', subtitle: 'The online audio distribution platform and music sharing.', icon: 'icons/soundcloud.svg', link: 'https://help.soundcloud.com/hc/articles/360054146474-How-can-I-share-music-from-SoundCloud-on-social-media-' },
-          { title: 'Amazon Music', subtitle: 'Reach more fans to your band page.', icon: 'icons/amazon_music.svg', link: 'https://music.amazon.com/' }
+          { title: 'Spotify', subtitle: 'Share your songs or podcasts.', icon: 'icons/spotify.com.svg', link: 'https://artists.spotify.com/help/article/sharing-your-music' },
+          { title: 'Apple music', subtitle: 'Get more listeners to your songs.', icon: 'icons/music.apple.com.svg', link: 'https://support.apple.com/guide/music-web/share-music-apdm0783785d/web' },
+          { title: 'Soundcloud', subtitle: 'The online audio distribution platform and music sharing.', icon: 'icons/soundcloud.com.svg', link: 'https://help.soundcloud.com/hc/articles/360054146474-How-can-I-share-music-from-SoundCloud-on-social-media-' },
+          { title: 'Amazon Music', subtitle: 'Reach more fans to your band page.', icon: 'icons/music.amazon.com.svg', link: 'https://music.amazon.com/' }
         ]
       },
       {
         title: 'Make and collect money',
         subtitle: 'Sell products, services, and collect tips',
         links: [
-          { title: 'Shopify', subtitle: 'Create your page and publish your products.', icon: 'icons/shopify.svg', link: 'https://www.shopify.com/' },
-          { title: 'MercadoLibre', subtitle: 'Sell your brand new or second hand items.', icon: 'icons/mercadolibre.svg', link: 'https://mercadolibre.com/' },
-          { title: 'Ebay', subtitle: 'Online e-commerce to sell your products online.', icon: 'icons/ebay.svg', link: 'https://www.ebay.com/' },
-          { title: 'Amazon', subtitle: 'Promote your items or wishlist.', icon: 'icons/amazon.svg', link: 'https://www.amazon.com/gp/help/customer/display.html?nodeId=GPWMKQXXSBT8NAHN' },
-          { title: 'OnlyFans', subtitle: 'Sell private and premium content.', icon: 'icons/onlyfans.svg', link: 'https://onlyfans.com/about.html' },
-          { title: 'Paypal', subtitle: 'Ask for payments or tips using credit card.', icon: 'icons/paypal.svg', link: 'https://paypal.com/' }
+          { title: 'Shopify', subtitle: 'Create your page and publish your products.', icon: 'icons/shopify.com.svg', link: 'https://www.shopify.com/' },
+          { title: 'MercadoLibre', subtitle: 'Sell your brand new or second hand items.', icon: 'icons/mercadolibre.com.mx.svg', link: 'https://mercadolibre.com/' },
+          { title: 'Ebay', subtitle: 'Online e-commerce to sell your products online.', icon: 'icons/ebay.com.svg', link: 'https://www.ebay.com/' },
+          { title: 'Amazon', subtitle: 'Promote your items or wishlist.', icon: 'icons/amazon.com.svg', link: 'https://www.amazon.com/gp/help/customer/display.html?nodeId=GPWMKQXXSBT8NAHN' },
+          { title: 'OnlyFans', subtitle: 'Sell private and premium content.', icon: 'icons/onlyfans.com.svg', link: 'https://onlyfans.com/about.html' },
+          { title: 'Paypal', subtitle: 'Ask for payments or tips using credit card.', icon: 'icons/paypal.com.svg', link: 'https://paypal.com/' }
         ]
       },
       {
         title: 'Collect information from your followers',
         subtitle: 'Grow, manage, and connect with your audience',
         links: [
-          { title: 'Typeform', subtitle: 'Online form building and online surveys.', icon: 'icons/typeform.svg', link: 'https://www.typeform.com/' },
-          { title: 'Reddit', subtitle: 'Social news aggregation, content rating, and discussion website.', icon: 'icons/reddit.svg', link: 'https://www.reddit.com/' },
-          { title: 'Whatsapp', subtitle: 'Connect with your audience.', icon: 'icons/whatsapp.svg', link: 'https://faq.whatsapp.com/452366545421244' },
-          { title: 'Telegram', subtitle: 'Create a group and share it to your followers.', icon: 'icons/telegram.svg', link: 'https://telegram.org/faq?setln=en#q-how-does-t-me-work' },
+          { title: 'Typeform', subtitle: 'Online form building and online surveys.', icon: 'icons/typeform.com.svg', link: 'https://www.typeform.com/' },
+          { title: 'Reddit', subtitle: 'Social news aggregation, content rating, and discussion website.', icon: 'icons/reddit.com.svg', link: 'https://www.reddit.com/' },
+          { title: 'Whatsapp', subtitle: 'Connect with your audience.', icon: 'icons/wa.me.svg', link: 'https://faq.whatsapp.com/452366545421244' },
+          { title: 'Telegram', subtitle: 'Create a group and share it to your followers.', icon: 'icons/t.me.svg', link: 'https://telegram.org/faq?setln=en#q-how-does-t-me-work' },
         ]
       }
     ].freeze
