cdf03fa7840ca05c53000d0d2c9183b8b74a9a2f
Luis Mendieta <luismepa@gmail.com>
2022-08-30 23:55:11 -0500

    feat: resize images before upload

--- app/javascript/packs/activestorage.js
+++ app/javascript/packs/activestorage.js
@@ -1,2 +1,40 @@
+import Compress from "compress.js"
 import * as ActiveStorage from "@rails/activestorage"
+
 ActiveStorage.start()
+
+addEventListener('change', async (event) => {
+    optimizeFileImageInputs(event)
+})
+
+async function optimizeFileImageInputs(event) {
+    const optimizeInputs = ['page_profile[profile_image]', 'page_profile[header_image]', 'content_item[custom_thumbnail_image]']
+    if (optimizeInputs.includes(event.target.name)) {
+        // event.target.form
+        const files = [...event.target.files]
+        const compress = new Compress();
+        const imgData = await compress.compress(files, {
+            size: 4,
+            quality: 0.75,
+            maxWidth: 1920,
+            maxHeight: 1920,
+            resize: true,
+            rotate: false,
+        })
+        const {prefix, data, alt, ext} = imgData[0]
+        const newFile = await urlToFile(`${prefix}${data}`, alt, ext)
+        event.target.files = new FileListItems([newFile])
+    }
+}
+
+async function urlToFile(url, filename, mimeType) {
+    const res = await fetch(url)
+    const buf = await res.arrayBuffer()
+    return new File([buf], filename, {type: mimeType})
+}
+
+function FileListItems(files) {
+    const b = new ClipboardEvent("").clipboardData || new DataTransfer()
+    for (let i = 0, len = files.length; i < len; i++) b.items.add(files[i])
+    return b.files
+}
